87b9ae7b4d92f739de66db91b236a2a7206fcd7b
Miranda Wilson <mirandascwilson@gmail.com>
2021-04-14 16:39:05 +0100

    Refactor main method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,15 +7,7 @@ class GildedRose
 
   def update_all
     @items.each do |item|
-
-      if type(item) == :normal
-        quality_adjust_normal(item)
-      elsif type(item) == :brie
-        quality_adjust_brie(item)
-      elsif type(item) == :passes
-        quality_adjust_passes(item)
-      end
-
+      update_quality(item)
       update_sell_in(item)
     end
   end
@@ -24,34 +16,42 @@ class GildedRose
 
   def type(item)
     item_types = { brie: "Aged Brie",
-                  sulfuras: "Sulfuras, Hand of Ragnaros",
-                  passes: "Backstage passes to a TAFKAL80ETC concert"
-                  }
-    item_types.key(item.name) ? item_types.key(item.name) : :normal
+                   sulfuras: "Sulfuras, Hand of Ragnaros",
+                   passes: "Backstage passes to a TAFKAL80ETC concert" }
+    item_types.key(item.name) || :normal
   end
 
   def update_sell_in(item)
     item.sell_in -= (type(item) == :sulfuras ? 0 : 1)
   end
 
+  def update_quality(item)
+    case type(item)
+    when :normal
+      quality_adjust_normal(item)
+    when :brie
+      quality_adjust_brie(item)
+    when :passes
+      quality_adjust_passes(item)
+    end
+  end
+
   def quality_adjust_normal(item)
     return if item.quality == 0
 
-    if item.sell_in >= 0
-      item.quality -= 1
-    else
-      item.quality -= 2
-    end
+    item.quality -= (item.sell_in >= 0 ? 1 : 2)
     item.quality = 0 if item.quality < 0
   end
 
   def quality_adjust_brie(item)
     return if item.quality == 50
+
     item.quality += 1
   end
 
   def quality_adjust_passes(item)
     return if item.quality == 50
+
     if item.sell_in > 10
       item.quality += 1
     elsif (6..10).include?(item.sell_in)
@@ -63,5 +63,4 @@ class GildedRose
     end
     item.quality = 50 if item.quality > 50
   end
-
 end
