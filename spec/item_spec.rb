76f869fb638595d69c8c011ad6c9993bee9c5b35
Miranda Wilson <mirandascwilson@gmail.com>
2021-04-14 18:30:17 +0100

    Add a test for Items string representation

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,4 +3,8 @@
 require "item"
 
 describe Item do
+  it "shows the Item's details nicely"do
+    item = Item.new("+5 Dexterity Vest", 0, 20)
+    expect(item.to_s).to eq "+5 Dexterity Vest, 0, 20"
+  end
 end
