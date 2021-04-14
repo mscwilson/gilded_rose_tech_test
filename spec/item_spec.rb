2e9ad6105bb698636901de6773cc0474eec9dea5
Miranda Wilson <mirandascwilson@gmail.com>
2021-04-14 18:33:01 +0100

    Move test helper constants to spec helper file

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,7 +4,7 @@ require "item"
 
 describe Item do
   it "shows the Item's details nicely"do
-    item = Item.new("+5 Dexterity Vest", 0, 20)
-    expect(item.to_s).to eq "+5 Dexterity Vest, 0, 20"
+    item = Item.new("+5 Dexterity Vest", DEFAULT_SELL_IN, DEFAULT_QUALITY)
+    expect(item.to_s).to eq "+5 Dexterity Vest, #{DEFAULT_SELL_IN}, #{DEFAULT_QUALITY}"
   end
 end
